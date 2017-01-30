1b1121a28cb348cb1bbcb81a21b3d631e8c0ad2d
Sunil <sunhick@gmail.com>
2017-01-29 18:41:51 -0700

    added test file

--- Assignment-1/wordCount.py
+++ Assignment-1/wordCount.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Word count: 
+
+Counts the total number of words, lines and paragraphs in a given file.
+"""
+
+__author__ = "Sunil"
+__email__ = "suba5417@colorado.edu"
+
 import re
 import os
 import sys
@@ -10,38 +19,44 @@ class WordCount(object):
              self.filename = filename
              self.__wc()
         else:
-            raise Exception("File \"{0}\", doesn't exists!".format(self.filename))
+            raise Exception("File \"{0}\", doesn't exists!".format(filename))
 
     def __wc(self):
         with open(self.filename) as file:
             # Fix me: Should i ignore apostrophe, Abbrevations, Periods, Numbers(45.45)
-            data = file.read().lower().replace("\'", "").replace(".", "") #.replace("-", "")
-            self.words = re.findall('\w+', data)
+            data = file.read().lower().replace("\'", "").replace(".", "")\
+                    .replace("-", "")
+            # print(data)
+            self.words = len(re.findall(r'\w+', data))
+            # print(self.words)
 
             # Paragraphs are separated by atleast two new lines
-            self.paragraphs = re.findall('\n{2,}', data)
+            self.paragraphs = len(re.findall(r'\n{2,}', data)) + 1
 
             # Sentences are separated by a newline
-            self.sentences = re.findall('\n', data)
+            self.sentences = len(re.findall(r'\n', data))
 
     @property
     def Paragraphs(self):
-        return len(self.paragraphs)
+        return self.paragraphs
 
     @property
     def Sentences(self):
-        return len(self.sentences)
+        return self.sentences
 
     @property
     def Words(self):
-        return len(self.words)
+        return self.words
 
-def main(argv):
-    filename = argv[0]
-    wc = WordCount(filename)
-    print("Paragraphs:", wc.Paragraphs)
-    print("Sentences:", wc.Sentences)
-    print("Words:", wc.Words)
+def main(cmdline):
+    filename = cmdline[0]
+    try:
+        wc = WordCount(filename)
+        print("Paragraphs:", wc.Paragraphs)
+        print("Sentences:", wc.Sentences)
+        print("Words:", wc.Words)
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
