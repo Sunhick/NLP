e004e1bd3956f1e525773f582d6ecec7632419ca
Sunil <sunhick@gmail.com>
2017-01-30 19:48:40 -0700

    keep the last dot after abbreviations

--- Assignment-1/wordCount.py
+++ Assignment-1/wordCount.py
@@ -45,7 +45,8 @@ class WordCount(object):
 
     def removeDottedAbbr(self, text):
         # Collapse abbrevations like U.S.A => USA, m.p.h => mph etc
-        removeDottedAbbr = re.compile(r"((?:[a-zA-Z]\.){2,})")
+        # removeDottedAbbr = re.compile(r"((?:[a-zA-Z]\.){2,})") # doesn't keep the last dot 
+        removeDottedAbbr = re.compile(r"(?:[a-zA-Z]\.[a-zA-Z]?){2,}") # keeps the last dot
         return removeDottedAbbr.sub(self.__removeDottedAbbr, text)
 
     def removePunctutationsInNumbers(self, text):
