c0002f518a905d9fbae89cdb8b98f663544e1264
Sunil <sunhick@gmail.com>
2017-02-16 19:56:07 -0700

    scaffolding for ngram model

---
+++ Assignment-2/ngram_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+N-gram language model. 
+
+Build a unigram and bigram model with and without smoothing.
+"""
+
+__author__ = "Sunil"
+__email__ = "suba5417@colorado.edu"
+
+import re
+import os
+import sys
+
+def GetUnigramCounts():
+    pass
+
+def GetBigramCounts():
+    pass
+
+# Or GetUnigramAndBigramCounts()
+def GetUnigramProbabilities():
+    pass
+
+def GetBigramProbabilities():
+    pass
+
+def GetSmoothedProbabilities():
+    pass
+
+# Or GetBigramAndSmoothedProb()
+def GetUnigramSentenceProbability():
+    pass
+
+def GetBigramSentenceProbability():
+    pass
+
+def GetSmoothedSentenceProbability():
+    pass
+
+def main(cmdline):
+    fileName = cmdline[0]
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
